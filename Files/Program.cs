3df3525d090a42284c87c096d0950dc1863c9b4b
Rilax <110360915+RilaxYT@users.noreply.github.com>
2022-08-01 03:30:43 +0200

    Update Program.cs

--- Files/Program.cs
+++ Files/Program.cs
@@ -1,39 +1,37 @@
 using System;
 using System.Diagnostics;
 
-namespace _3x_1
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int x;
+        Stopwatch stopwatch = new Stopwatch();
+
+        Console.WriteLine("Start!");
+
+        stopwatch.Start();
+        for (int i = 1; i <= 1000000; i++)
         {
-            int x;
-            Stopwatch stopwatch = new Stopwatch();
-
-            Console.WriteLine("Start!");
-
-            stopwatch.Start();
-            for (int i = 1; i <= 1000000; i++)
+            x = i;
+            while (true)
             {
-                x = i;
-                while (true)
+                if (x <= 1)
                 {
-                    if (x <= 1)
-                    {
-                        break;
-                    }
-                    else if (x % 2 == 1)
-                    {
-                        x = 3 * x + 1;
-                    }
-                    else if (x % 2 == 0)
-                    {
-                        x /= 2;
-                    }
+                    break;
+                }
+                else if (x % 2 == 1)
+                {
+                    x = 3 * x + 1;
+                }
+                else if (x % 2 == 0)
+                {
+                    x /= 2;
                 }
             }
-            stopwatch.Stop();
-            Console.WriteLine("End! This took " + (double) stopwatch.ElapsedMilliseconds/1000 +  "seconds!");
         }
+
+        stopwatch.Stop();
+        Console.WriteLine("End! This took " + (double)stopwatch.ElapsedMilliseconds / 1000 + "seconds!");
     }
 }
