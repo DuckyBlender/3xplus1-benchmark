fa25efe49313d32e78c503a212e6f6c2ba2f0b60
Rilax <110360915+RilaxYT@users.noreply.github.com>
2022-08-01 03:25:08 +0200

    Create Main.java

---
+++ Files/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        int x;
+        System.out.println("Start!");
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 1; i <= 1000000; i++)
+        {
+            x = i;
+            while (true) {
+                if (x <= 1) {
+                    break;
+                } else if (x % 2 == 1) {
+                    x = 3 * x + 1;
+                } else if (x % 2 == 0) {
+                    x /= 2;
+                }
+            }
+        }
+
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println("End! This took " + (double) elapsedTime/1000 + "s");
+    }
+}
